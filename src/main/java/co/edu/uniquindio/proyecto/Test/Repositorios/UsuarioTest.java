b2b6199f1f0c5512e0857b79dc57581163a68280
Cdrian18 <valenciacarlos1218@gmail.com>
2023-10-19 09:22:03 -0500

    Test del usuarioRepo

--- src/main/java/co/edu/uniquindio/proyecto/Test/Repositorios/UsuarioTest.java
+++ src/main/java/co/edu/uniquindio/proyecto/Test/Repositorios/UsuarioTest.java
@@ -1,4 +1,4 @@
-package co.edu.uniquindio.proyecto.Test;
+package co.edu.uniquindio.proyecto.Test.Repositorios;
 
 import co.edu.uniquindio.proyecto.modelo.entidades.*;
 import co.edu.uniquindio.proyecto.repositorios.UsuarioRepo;
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 
 @DataJpaTest
@@ -26,6 +27,17 @@ public class UsuarioTest {
         Usuario guardado = usuarioRepo.save(u);
         //Comprobamos que si haya quedado
         Assertions.assertNotNull(guardado);
+    }
+    //Test para actualizar
+    @Test
+    @Sql("classpath:pruebas.sql")
+    public void actualizarUsuarioTest() {
+    Usuario u = usuarioRepo.findByEmail("emai@email.com").orElse(null);
+    System.out.println("Nombre antiguo: " + u.getNombre());
+    u.setNombre("Juanita");
+    usuarioRepo.save(u);
+    System.out.println(u.getNombre());
+    Assertions.assertEquals("Juanita", u.getNombre());
     }
 
 }
